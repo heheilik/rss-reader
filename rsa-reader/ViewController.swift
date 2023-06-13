88509641ae68fb08d11414a253159395bd7208a2
Heorhi Heilik <heheilik@icloud.com>
2023-06-13 14:38:35 +0300

    Initial commit
    Template project created

---
+++ rsa-reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rsa-reader
+//
+//  Created by Heorhi Heilik on 13.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
