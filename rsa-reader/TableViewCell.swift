94cf7734304fe65e4eb741a301dfb62579423f3f
Heorhi Heilik <heheilik@icloud.com>
2023-06-13 23:07:32 +0300

    Added UITableView with custom cells

---
+++ rsa-reader/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  rsa-reader
+//
+//  Created by Heorhi Heilik on 13.06.23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var centerLabel: UILabel!
+    
+}
