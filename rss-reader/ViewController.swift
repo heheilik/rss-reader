a913a2086d0bf39df8c3cfe503b46854ff8cf3b5
Heorhi Heilik <heheilik@icloud.com>
2023-07-06 03:17:18 +0300

    XMLCoder dependency removed

--- rss-reader/ViewController.swift
+++ rss-reader/ViewController.swift
@@ -6,23 +6,22 @@
 //
 
 import UIKit
-import XMLCoder
 
-struct Feed: Codable {
-    @Attribute var xmlns: String
-    @Element var title: String
-    @Element var updated: String
-    @Element var id: String
-    var entry: [Entry]
-}
-
-struct Entry: Codable {
-    @Element var title: String
-    @Element var author: String
-    @Element var updated: String
-    @Element var id: String
-    @Element var content: String
-}
+//struct Feed: Codable {
+//    @Attribute var xmlns: String
+//    @Element var title: String
+//    @Element var updated: String
+//    @Element var id: String
+//    var entry: [Entry]
+//}
+//
+//struct Entry: Codable {
+//    @Element var title: String
+//    @Element var author: String
+//    @Element var updated: String
+//    @Element var id: String
+//    @Element var content: String
+//}
 
 class ViewController: UIViewController {
     
@@ -43,12 +42,6 @@ class ViewController: UIViewController {
             guard let data else {
                 print("No data downloaded.")
                 return
-            }
-            
-            let decoded = try! XMLDecoder().decode(Feed.self, from: data)
-            print(decoded.xmlns, decoded.title, decoded.updated, decoded.id, separator: "\n")
-            for i in 0..<decoded.entry.count {
-                print("\(i + 1) - \(decoded.entry[i].title)")
             }
         }
         downloadTask.resume()
