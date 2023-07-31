b76b81f6062654041f4d5731025d95c92d4b8f27
Heorhi Heilik <heheilik@icloud.com>
2023-07-31 03:06:48 +0300

    Data moved to FeedService

--- rss-reader/Feed/FeedService.swift
+++ rss-reader/Feed/FeedService.swift
@@ -7,9 +7,34 @@
 
 import UIKit
 
-final class FeedService {
+final class FeedService: NSObject {
     
-    func prepareFeed(with url: URL, completion: @escaping (Feed?) -> Void) {
+    override init() {
+        super.init()
+        if feedUrls.count == 0 {
+            feedUrls.append("https://www.swift.org/atom.xml")
+        }
+        feed = .init(repeating: nil, count: feedUrls.count)
+    }
+    
+    var activeFeedIndex = 0
+    private(set) var feed: [Feed?] = []  // TODO: make list instead of array
+    
+    var feedUrls: [String] {
+        get {
+            UserDefaults.standard.array(forKey: "urls") as? [String] ?? []
+        }
+        set {
+            UserDefaults.standard.setValue(newValue, forKey: "urls")
+        }
+    }
+    
+    func prepareFeed(withIndex index: Int, completion: @escaping (Feed?) -> Void) {
+        guard let url = URL(string: feedUrls[index]) else {
+            print("URL is not available.")
+            return
+        }
+        
         let downloadTask = URLSession.shared.dataTask(with: url) { data, response, error in
             guard error == nil else {
                 print(error ?? "Unknown error.")
@@ -22,10 +47,40 @@ final class FeedService {
             
             let parser = Parser()
             parser.parse(data) { feed in
+                self.feed[self.activeFeedIndex] = feed
                 completion(feed)
             }
         }
         downloadTask.resume()
+    }
+    
+}
+
+extension FeedService: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return feed[activeFeedIndex]?.entry.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RssInfoTableViewCell", for: indexPath) as? RssInfoTableViewCell else {
+            fatalError("Failed to dequeue.")
+        }
+        
+        if let entry = feed[activeFeedIndex]?.entry[indexPath.row] {
+            cell.updateContentsWith(entry)
+        } else {
+            let emptyEntry = Entry(
+                title: "No data available.",
+                author: "-",
+                updated: "-",
+                id: "-",
+                content: ""
+            )
+            cell.updateContentsWith(emptyEntry)
+        }
+        
+        return cell
     }
     
 }
--- rss-reader/MainScreen/ViewController.swift
+++ rss-reader/MainScreen/ViewController.swift
@@ -9,18 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentFeedName = "Swift"
-    var feed = ["Swift": Feed()]
     let feedService = FeedService()
-    
-    var feedUrls: [String] {
-        get {
-            UserDefaults.standard.array(forKey: "urls") as? [String] ?? []
-        }
-        set {
-            UserDefaults.standard.setValue(newValue, forKey: "urls")
-        }
-    }
     
     @IBOutlet weak var feedTitle: UILabel!
     @IBOutlet weak var table: UITableView!
@@ -29,15 +18,10 @@ class ViewController: UIViewController {
     // MARK: - lifecycle
     
     override func viewDidLoad() {
-        if feedUrls.isEmpty {
-            feedUrls.append("https://www.swift.org/atom.xml")
-        }
-        
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        table.delegate = self
-        table.dataSource = self
+        table.dataSource = feedService
         table.register(UINib(nibName: "RssInfoTableViewCell", bundle: nil), forCellReuseIdentifier: "RssInfoTableViewCell")
     }
     
@@ -45,46 +29,12 @@ class ViewController: UIViewController {
     // MARK: - IBActions
     
     @IBAction func downloadFeedTouchUpInside() {
-        guard let url = URL(string: feedUrls[0]) else {
-            print("URL is not available.")
-            return
-        }
-        feedService.prepareFeed(with: url) { feed in
-            self.feed[self.currentFeedName] = feed
-            DispatchQueue.main.async {
-                self.feedTitle.text = self.feed[self.currentFeedName]?.title.trimmingCharacters(in: .whitespacesAndNewlines) ?? "[feed-title]"
-                self.table.reloadData()
+        feedService.prepareFeed(withIndex: feedService.activeFeedIndex) { feed in
+            DispatchQueue.main.async { [self] in
+                feedTitle.text = feedService.feed[feedService.activeFeedIndex]?.title.trimmingCharacters(in: .whitespacesAndNewlines) ?? "[feed-title]"
+                table.reloadData()
             }
         }
     }
 
-}
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return feed[currentFeedName]?.entry.count ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = table.dequeueReusableCell(withIdentifier: "RssInfoTableViewCell", for: indexPath) as? RssInfoTableViewCell else {
-            fatalError("Failed to dequeue.")
-        }
-        
-        if let entry = feed[currentFeedName]?.entry[indexPath.row] {
-            cell.updateContentsWith(entry)
-        } else {
-            let emptyEntry = Entry(
-                title: "No data available.",
-                author: "-",
-                updated: "-",
-                id: "-",
-                content: ""
-            )
-            cell.updateContentsWith(emptyEntry)
-        }
-        
-        return cell
-    }
-    
 }
