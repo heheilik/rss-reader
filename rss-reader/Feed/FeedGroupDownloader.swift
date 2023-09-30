ebb1896e2270f5ec59d9344d02ee1268ac866463
Heorhi Heilik <heheilik@icloud.com>
2023-09-30 13:47:06 +0300

    Download completion handler rewritten

--- rss-reader/Feed/FeedGroupDownloader.swift
+++ rss-reader/Feed/FeedGroupDownloader.swift
@@ -13,10 +13,11 @@ class FeedGroupDownloader {
     private let feedHttpService: FeedHttpService
     private let urlsToDownload: Set<URL>
 
-    private var completionHandlers = [(ParsedFeed?) -> Void]()
-    private var parsedFeedsForCompletionHandlers = [ParsedFeed?]()
+    private var parsedFeedForUrl = [URL: ParsedFeed?]()
 
     var context: NSManagedObjectContext?
+
+    // TODO: Add context save completion handler
 
     init(feedHttpService: FeedHttpService, urlsToDownload: Set<URL>) {
         self.feedHttpService = feedHttpService
@@ -26,65 +27,58 @@ class FeedGroupDownloader {
     func download() {
         for url in urlsToDownload {
             feedHttpService.prepareFeed(withURL: url) { [self] parsedFeed in
-                // FIXME: Strong reference cycle!!!
-                let completionHandler: (ParsedFeed?) -> Void = { [self] parsedFeed in
-//                    guard let strongSelf = self else {
-//                        return
-//                    }
-//                    guard let context = strongSelf.context else {
-//                        fatalError("Data processing started when context was unavailable.")
-//                    }
-                    guard let context = self.context else {
-                        fatalError("Data processing started when context was unavailable.")
-                    }
-
-                    context.perform {
-                        defer {
-                            do {
-                                if context.hasChanges {
-                                    try context.save()
-                                }
-                            } catch {
-                                print("Error saving child context. \(error)")
-                            }
-                        }
-
-                        guard let parsedFeed else {
-                            return
-                        }
-
-                        guard let feed = context.registeredObjects.first(where: { object in
-                            guard let feed = object as? Feed else {
-                                return false
-                            }
-                            return feed.url == url
-                        }) as? Feed else {
-                            let feed = Feed(context: context)
-                            feed.setData(from: parsedFeed, forUrl: url)
-                            return
-                        }
-
-                        guard feed.identifier == parsedFeed.header.identifier else {
-                            feed.setData(from: parsedFeed, forUrl: url)
-                            return
-                        }
-
-                    }
+                guard context != nil else {
+                    parsedFeedForUrl[url] = parsedFeed
+                    return
                 }
-
-                if self.context != nil {
-                    completionHandler(parsedFeed)
-                } else {
-                    self.parsedFeedsForCompletionHandlers.append(parsedFeed)
-                    self.completionHandlers.append(completionHandler)
-                }
+                urlCompletedLoading(url: url, parsedFeed: parsedFeed)
             }
         }
     }
 
+    func urlCompletedLoading(url: URL, parsedFeed: ParsedFeed?) {
+        guard let context else {
+            fatalError("Data processing started when context was unavailable.")
+        }
+
+        context.perform {
+            defer {
+                do {
+                    if context.hasChanges {
+                        try context.save()
+                    }
+                } catch {
+                    print("Error saving child context. \(error)")
+                }
+            }
+
+            guard let parsedFeed else {
+                return
+            }
+
+            // TODO: get feed and entries with fetch request
+            guard let feed = context.registeredObjects.first(where: { object in
+                guard let feed = object as? Feed else {
+                    return false
+                }
+                return feed.url == url
+            }) as? Feed else {
+                let feed = Feed(context: context)
+                feed.setData(from: parsedFeed, forUrl: url)
+                return
+            }
+
+            guard feed.identifier == parsedFeed.header.identifier else {
+                feed.setData(from: parsedFeed, forUrl: url)
+                return
+            }
+
+        }
+    }
+
     func startCompletionHandlers() {
-        for index in completionHandlers.indices {
-            completionHandlers[index](parsedFeedsForCompletionHandlers[index])
+        for (url, parsedFeed) in parsedFeedForUrl {
+            urlCompletedLoading(url: url, parsedFeed: parsedFeed)
         }
     }
 
